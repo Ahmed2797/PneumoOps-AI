ba8b8c950596b1e4063503447b9a9a9c2d7b5fd7
Ahmed <tanvirahmed754575@gmail.com>
2026-05-02 13:29:31 +0300

    full pipeline

---
+++ src/pipeline/__init__.py
@@ -0,0 +1,73 @@
+from pathlib import Path
+import sys
+
+from src.components.data_ingestion import Data_Ingestion
+
+from src.configeration import Configeration_Manager
+from src.exception import CustomException
+from src.logger import logging
+
+
+class Training_Pipeline:
+    """
+    Orchestrates the complete machine learning workflow including:
+    - Data ingestion
+    - Base model preparation
+    - Callback creation
+    - Model training
+    - Model evaluation
+    
+    This class executes each pipeline step sequentially using configurations
+    provided by the ConfigerationManager.
+    """
+
+    def __init__(self):
+        """
+        Initialize the TrainingPipeline with a single instance of
+        ConfigerationManager to access all configuration sections.
+        """
+        self.config = Configeration_Manager()
+
+
+    def run_data_ingestion(self):
+        """
+        Execute the data ingestion pipeline.
+        
+        Steps:
+        - Download the dataset from the specified URL.
+        - Extract the downloaded zip file.
+        
+        Raises:
+            CustomException: If any part of ingestion fails.
+        """
+        try:
+            logging.info(">>>>>>> Data Ingestion started <<<<<<<<<")
+            data_ingestion_config = self.config.get_data_ingestion_config()
+            data_ingestion = Data_Ingestion(data_ingestion_config)
+            data_ingestion.run()
+            logging.info(">>>>>>> Data Ingestion completed <<<<<<<<<")
+        except Exception as e:
+            raise CustomException(e, sys)
+
+
+
+    def run(self):
+        """
+        Execute the full ML pipeline in order:
+        1. Data ingestion
+        2. Base model preparation
+        3. Model training
+        4. Model evaluation
+        
+        Raises:
+            CustomException: If any stage of the pipeline fails.
+        """
+        try:
+            logging.info(">>>>>>> Training Pipeline started <<<<<<<<<")
+            self.run_data_ingestion()
+        
+            logging.info(">>>>>>> Training Pipeline completed <<<<<<<<<")
+        except Exception as e:
+            raise CustomException(e, sys)
+
+
