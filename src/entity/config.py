efcba6b10c55afd5841acf42154e953179ded6c2
Ahmed <tanvirahmed754575@gmail.com>
2026-05-03 09:32:09 +0300

    preparebasemodel entity

--- src/entity/config.py
+++ src/entity/config.py
@@ -26,3 +26,30 @@ class Data_Ingestion_Config:
     bucket_name: str
     object_key: str
     region_name: str
+
+
+
+@dataclass(frozen=True)
+class Prepare_Basemodel_Config:
+    """
+    Dataclass for storing the configuration required to prepare the base model.
+
+    Attributes:
+        root_dir (Path): Base directory for base model artifacts.
+        base_model (Path): Path to the base model file.
+        update_base_model (Path): Path to the updated base model file.
+        param_image_size (list): List of image sizes for training.
+        param_batch_size (int): Batch size for training.
+        param_epochs (int): Number of epochs for training.
+    """
+    root_dir: Path
+    base_model: Path
+    update_base_model: Path 
+    param_image_size: list
+    param_batch_size: int
+    param_epochs: int 
+    param_learning_rate: float
+    param_classics: int
+    param_weight:str
+    param_include_top: bool
+
